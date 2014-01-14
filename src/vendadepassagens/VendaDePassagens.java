6d881382afe3699108a39fb4765527aa094fb89c
eulersantana <eulersantana@eulersantana-HP-Pavilion-dv7-Notebook-PC>
2014-01-14 16:04:51 -0300

    ajuste da main

--- src/vendadepassagens/VendaDePassagens.java
+++ src/vendadepassagens/VendaDePassagens.java
@@ -15,6 +15,6 @@ public class VendaDePassagens {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("ENG II");
+        
     }
 }
