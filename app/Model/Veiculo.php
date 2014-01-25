3224cd7dc4363638a3f6cc286512fd43e35fe90c
tercio.souza <terciosouza@genesiovalois-gere.(none)>
2014-01-25 17:55:49 -0300

    comitando o arquivo Veiculo.php . Pois tem a relação com Rota.

--- app/Model/Veiculo.php
+++ app/Model/Veiculo.php
@@ -1,6 +1,14 @@
 <?php
 	class Veiculo extends AppModel {
 		public $name = 'Veiculo';
-	    public $hasOne = 'Rota';
+	   	//public $hasOne = 'Rota';
+
+	   	public $hasOne = array(
+	   		'Rota' => array(
+	   			'className' => 'Rota',
+	   			'foreignKey' => 'id',
+	   			'conditions' => array()
+	   			)
+	   		);
 	}
 ?>
